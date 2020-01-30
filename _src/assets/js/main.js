1a6188816df48fdeffe0631d1d743b6078e018e2
elenapayan <elena.payan.glez@gmail.com>
2020-01-30 18:06:51 +0100

    Add listeners to the images, button close and overlay

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -22,13 +22,34 @@ window.addEventListener("load", () => {
 
       const category = ev.target.innerHTML.toLowerCase();
       category === "all" ? grid.filter("[data-categories]") : grid.filter(`[data-categories="${category}"]`);
-    })
+    });
   });
 
   //Filtro por etiqueta en el input//
   document.querySelector("#header-input").addEventListener("input", ev => {
     const search = ev.target.value;
     grid.filter(item => item.getElement().dataset.label.includes(search));
-  })
+  });
 
-})
+  //Listeners en las imágenes//
+  const overlay = document.getElementById("overlay");
+  document.querySelectorAll(".grid-items-images").forEach(element => {
+    element.addEventListener("click", () => {
+      const route = element.getAttribute("src");
+      const description = element.parentNode.parentNode.dataset.description;
+      overlay.classList.add("active");
+      document.querySelector(".overlay-image").src = route;
+      document.querySelector(".overlay-description").innerHTML = description;
+    });
+  });
+
+  //Listeners en el botón de cerrar//
+  document.querySelector("#btn-close").addEventListener("click", () => {
+    overlay.classList.remove("active");
+  });
+
+  //Listeners del overlay//
+  overlay.addEventListener("click", ev => {
+    ev.target.id === "overlay" ? overlay.classList.remove("active") : "";
+  })
+});
